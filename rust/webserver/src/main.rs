f239a5ffe54ef93c814a74b15059a021cef765d9
Vlad Micu <vlad.abcd@gmail.com>
2026-04-06 15:57:13 +0300

    responding to request

--- rust/webserver/src/main.rs
+++ rust/webserver/src/main.rs
@@ -1,9 +1,21 @@
-use std::net::TcpListener;
+use std::{io::Write, net::{TcpListener, TcpStream}};
 
 fn main() -> std::io::Result<()>{
     let listener = TcpListener::bind("127.0.0.1:8080")?;
-    for _stream in listener.incoming(){
-        println!("Recieved something"); //TODO: handle request 
+    for stream in listener.incoming(){
+        handle_request(&mut stream?);
     }
     Ok(())
+}
+
+fn handle_request(stream: &mut TcpStream){
+    //TODO: use request in response
+    let response = "<html><body>Hello World!</body></html>\n".as_bytes();
+    let len = response.len();
+    let _ = stream.write("HTTP/1.1 200 OK\n".as_bytes());
+    let _ = stream.write("content-type: text/html\n".as_bytes());
+    let _ = stream.write(format!("content-length: {len}\n").as_bytes());
+    let _ = stream.write("\n".as_bytes());
+    let _ = stream.write(response);
+    let _ = stream.flush();
 }
