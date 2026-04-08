0a40cc3fcc024d6a1c36e30b1b7bebbc10899bc2
Vlad Micu <vlad.abcd@gmail.com>
2026-04-08 20:08:29 +0300

    better code localization of error handling + using &str instead of String for storing request data

--- rust/webserver/src/main.rs
+++ rust/webserver/src/main.rs
@@ -1,5 +1,5 @@
 use core::fmt;
-use std::{collections::HashMap, io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}};
+use std::{collections::HashMap, io::{Read, Write}, net::{TcpListener, TcpStream}};
 
 fn main() -> std::io::Result<()>{
     let listener = TcpListener::bind("127.0.0.1:8080")?;
@@ -10,9 +10,26 @@ fn main() -> std::io::Result<()>{
 }
 
 fn handle_request(stream: &mut TcpStream){
-    let mut reader = BufReader::new(stream);
-    let req = parse_and_validate(&mut reader);
-    let stream = reader.into_inner();
+    let mut raw_request_bytes : Vec<u8> = vec![];
+    let mut buff: [u8; 1000] = [0;1000];
+    let mut read_result = stream.read(&mut buff);
+    while let Ok(n) = read_result{
+        raw_request_bytes.extend_from_slice(&buff[0..n]);
+        if n < buff.len() {
+            break;
+        }
+        //buff = [0; 1000]; //doesn't seem necessary, tested with smaller buffer size that requires multiple iterations to read entire request 
+        read_result = stream.read(&mut buff);
+    }
+    let Ok(_) = read_result else{
+        return error_response(stream, HttpError::InternalServerError);
+    };
+    let Ok(raw_request) = str::from_utf8(&raw_request_bytes) else{
+        return error_response(stream, HttpError::BadRequest);
+    };
+    //println!("Request:");
+    //println!("{raw_request}");
+    let req = Request::parse(raw_request);
     match req{
         Err(err) => error_response(stream, err),
         Ok(r) => normal_response(stream, r)
@@ -25,13 +42,16 @@ fn error_response(stream: &mut TcpStream, err: HttpError){
 }
 
 fn normal_response(stream: &mut TcpStream, req : Request){
-    let name = match req.headers.get("name"){
-        Some(s) => s,
-        None => match req.params.get("name"){
-            Some(s) => s,
-            None => "World"
-        }
-    };
+    if req.resource != "/" {
+        return error_response(stream, HttpError::NotFound);
+    }
+    if req.method != HttpMethod::GET {
+        return error_response(stream, HttpError::MethodNotAllowed);
+    }
+
+    let name =  req.headers.get("name")
+                            .or(req.params.get("name"))
+                            .unwrap_or(&"World");
     let str_content = format!("<html><body>Hello {name}!</body></html>\n");
     let content = str_content.as_bytes();
     let content_length = content.len();
@@ -43,6 +63,7 @@ fn normal_response(stream: &mut TcpStream, req : Request){
     let _ = stream.flush();
 }
 
+#[derive(PartialEq)]
 enum HttpMethod{
     GET,
     HEAD,
@@ -55,8 +76,8 @@ enum HttpMethod{
     TRACE
 }
 
-fn get_method(name: String) -> Option<HttpMethod>{
-    match name.as_str() {
+fn get_method(name: &str) -> Option<HttpMethod>{
+    match name {
         "GET" => Some(HttpMethod::GET),
         "HEAD" => Some(HttpMethod::HEAD),
         "POST" => Some(HttpMethod::POST),
@@ -70,18 +91,71 @@ fn get_method(name: String) -> Option<HttpMethod>{
     }
 }
 
-struct Request{
+struct Request<'a>{
     method: HttpMethod,
-    resource: String,
-    headers: HashMap<String, String>,
-    params: HashMap<String, String>
+    resource: &'a str,
+    headers: HashMap<&'a str, &'a str>,
+    params: HashMap<&'a str, &'a str>,
+    content: &'a str
+}
+
+impl<'a> Request<'a> {
+    fn parse(raw_request: &'a str) -> Result<Self, HttpError>{
+        let delimiter = raw_request.find("\n\n")
+                        .or_else(|| raw_request.find("\r\n\r\n"));
+        let (raw_headers, content) = match delimiter{
+            None => (raw_request, ""),
+            Some(index) => raw_request.split_at(index)
+        };
+        let mut lines = raw_headers.lines();
+
+        let first_line = lines.nth(0).ok_or(HttpError::BadRequest)?;
+        let mut parts = first_line.split_whitespace();
+        let raw_method = parts.nth(0).ok_or(HttpError::BadRequest)?;
+        let res_and_params = parts.nth(0).ok_or(HttpError::BadRequest)?;
+        let method = get_method(raw_method).ok_or(HttpError::NotImplemented)?;
+        let param_delimiter = res_and_params.find('?');
+        let (resource, raw_parameters) = match param_delimiter {
+            None => (res_and_params, ""),
+            Some(index) => res_and_params.split_at(index)
+        };
+        let params : HashMap<&str, &str> = match raw_parameters{
+            "" => HashMap::new(),
+            _ => raw_parameters.trim_start_matches('?')
+                               .split('&')
+                               .map(|line| {
+                                    line.find('=').map(|index| line.split_at(index))
+                                                  .map(|(key, value)| (key, value.trim_start_matches('=')))
+
+                                })
+                               .filter(Option::is_some)
+                               .map(Option::unwrap)
+                               .collect()
+        };
+        let headers : HashMap<&str, &str> = lines.map(|line|{
+                                                        line.find(": ").map(|index| line.split_at(index))
+                                                                       .map(|(key, value)| (key, value.trim_start_matches(": ")))
+                                                    }) 
+                                                    .filter(Option::is_some)
+                                                    .map(Option::unwrap)
+                                                    .collect();
+        let request = Request{
+            method: method,
+            resource: resource,
+            headers: headers,
+            params: params,
+            content: content
+        };
+        Ok(request)
+    }
 }
 
 enum HttpError{
     NotFound,
     NotImplemented,
     MethodNotAllowed,
-    BadRequest
+    BadRequest,
+    InternalServerError
 }
 
 impl fmt::Display for HttpError{
@@ -90,106 +164,9 @@ impl fmt::Display for HttpError{
             HttpError::NotFound => "404 Not Found",
             HttpError::NotImplemented => "501 Not Implemented",
             HttpError::MethodNotAllowed => "405 Method Not Allowed",
-            HttpError::BadRequest => "400 Bad Request"
+            HttpError::BadRequest => "400 Bad Request",
+            HttpError::InternalServerError => "500 Internal Server Error"
         };
         write!(f, "{msg}")
     }
-}
-
-fn parse_and_validate(reader: &mut BufReader<&mut TcpStream>) -> Result<Request, HttpError>{
-    let mut first_line = String::new();
-    let len = reader.read_line(&mut first_line);
-    match len {
-        Err(_) => return Err(HttpError::BadRequest),
-        Ok(sz) => if sz == 0 {return Err(HttpError::BadRequest);} else {()}
-    }
-    let parts : Vec<&str> = first_line.split(' ').collect();
-    if parts.len() < 2 {
-        return Err(HttpError::BadRequest);
-    }
-    let res_and_params: Vec<&str> = parts[1].split('?').collect();
-    let resource = match res_and_params[0]{
-        "/" => String::from("/"),
-        _ => return Err(HttpError::NotFound)
-    };
-    let params = match get_params(res_and_params){
-        Err(e) => return Err(e),
-        Ok(p) => p
-    };
-
-    let method = match get_method(String::from(parts[0])){
-        None => return Err(HttpError::NotImplemented),
-        Some(m) => m
-    };
-    match method{
-        HttpMethod::GET => (),
-        _ => return Err(HttpError::MethodNotAllowed)
-    };
-    let headers = match get_headers(reader) {
-        Err(e) => return Err(e),
-        Ok(h) => h
-    };
-
-    let req = Request{
-        method: method,
-        resource: resource,
-        headers: headers,
-        params: params
-    };
-    Ok(req)
-} 
-
-fn get_headers(reader: &mut BufReader<&mut TcpStream>) -> Result<HashMap<String, String>, HttpError>{
-    let mut headers: HashMap<String, String> = HashMap::new();
-    let mut header = String::new();
-    let mut len = reader.read_line(&mut header);
-    let mut keep_going: bool = match len {
-        Err(_) => false,
-        Ok(l) => match l {
-            0 => false,
-            _ => true
-        }
-    };
-    while keep_going{
-        let key_val : Vec<String> = header.split(": ")
-                                          .map(String::from)
-                                          .collect();
-        if key_val.len() != 2 {
-            if key_val.len() > 2{
-                return Err(HttpError::BadRequest);
-            } else {
-                break;
-            }
-        }
-        let mut iter = key_val.into_iter();
-        headers.insert(iter.next().unwrap(), iter.next().unwrap());
-
-        header = String::new();
-        len = reader.read_line(&mut header);
-        keep_going = match len {
-            Err(_) => false,
-            Ok(l) => match l {
-                0 => false,
-                _ => true
-            }
-        };
-    }
-    Ok(headers)
-}
-
-fn get_params(res_and_params: Vec<&str>) -> Result<HashMap<String, String>, HttpError>{
-    let mut params: HashMap<String, String> = HashMap::new();
-    if res_and_params.len() > 1 {
-        for param in res_and_params[1].split('&'){
-            let key_val: Vec<String> = param.split('=')
-                                            .map(String::from)
-                                            .collect();
-            if key_val.len()  != 2 {
-                return Err(HttpError::BadRequest);
-            }
-            let mut iter = key_val.into_iter();
-            params.insert(iter.next().unwrap(), iter.next().unwrap());
-        }
-    }
-    Ok(params)
 }
